a686e3b294d10fdd58cfa220b7cd373d253fcdfe
dOrgJelli <jelli@dorg.tech>
2020-08-25 18:23:17 -0500

    added permission manager + create vote grantee

--- contracts/LidDaoTemplate.sol
+++ contracts/LidDaoTemplate.sol
@@ -1,8 +1,13 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.16;
 
+import "./grantees/LidVotingRightsGrantee.sol";
 import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 
 contract LidDaoTemplate is BaseTemplate {
+
+  // bytes32 constant internal LID_VR_GRANTEE_APP_ID = keccak256("lidvotingrightsgrantee");
+  bytes32 internal constant LID_VR_GRANTEE_APP_ID = 0x23e0763d95d9c50b5a659002f6845ab4ecee9630a09736ff770c781656b14167;
+
   string constant private ERROR_BAD_VOTE_SETTINGS = "LID_DAO_BAD_VOTE_SETTINGS";
 
   MiniMeToken public lidVotingRights;
@@ -28,7 +33,8 @@ contract LidDaoTemplate is BaseTemplate {
   */
   function newInstance(
     string memory _id,
-    uint64[3] memory _votingSettings
+    uint64[3] memory _votingSettings,
+    address _permissionManager
   )
   public
   {
@@ -36,39 +42,64 @@ contract LidDaoTemplate is BaseTemplate {
     _validateVotingSettings(_votingSettings);
 
     (Kernel dao, ACL acl) = _createDAO();
-    (Voting voting) = _setupApps(dao, acl, _votingSettings);
-    _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, voting);
+    (Voting voting) = _setupApps(dao, acl, _votingSettings, _permissionManager);
+    _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, _permissionManager);
     _registerID(_id, dao);
   }
 
   function _setupApps(
     Kernel _dao,
     ACL _acl,
-    uint64[3] memory _votingSettings
+    uint64[3] memory _votingSettings,
+    address _permissionManager
   )
   internal
   returns (Voting)
   {
     Agent agent = _installDefaultAgentApp(_dao);
+    LidVotingRightsGrantee vrGrantee = _installLidVotingRightsGrantee(_dao, lidVotingRights);
     Voting voting = _installVotingApp(_dao, lidVotingRights, _votingSettings);
 
-    _setupPermissions(_acl, agent, voting);
+    _setupPermissions(
+      _acl,
+      agent,
+      voting,
+      address(vrGrantee),
+      _permissionManager
+    );
 
     return (voting);
+  }
+
+  function _installLidVotingRightsGrantee(
+    Kernel _dao,
+    LidVotingRights _lidVotingRights
+  )
+  internal
+  returns (LidVotingRightsGrantee)
+  {
+    bytes memory initializeData = abi.encodeWithSelector(
+      LidVotingRightsGrantee(0).initialize.selector,
+      _lidVotingRights
+    );
+    return LidVotingRightsGrantee(
+      _installNonDefaultApp(_dao, LID_VR_GRANTEE_APP_ID, initializeData)
+    );
   }
 
   function _setupPermissions(
     ACL _acl,
     Agent _agent,
-    Voting _voting
+    Voting _voting,
+    address _createVotesGrantee,
+    address _permissionManager
   )
   internal
   {
-    _createAgentPermissions(_acl, _agent, _voting, _voting);
-    _createVaultPermissions(_acl, Vault(_agent), _voting, _voting);
-    _createEvmScriptsRegistryPermissions(_acl, _voting, _voting);
-    _createVotingPermissions(_acl, _voting, _voting, _voting, _voting);
-    _acl.createPermission(address(0), _voting, _voting.CREATE_VOTES_ROLE(), address(0));
+    _createAgentPermissions(_acl, _agent, _voting, _permissionManager);
+    _createVaultPermissions(_acl, Vault(_agent), _voting, _permissionManager);
+    _createEvmScriptsRegistryPermissions(_acl, _voting, _permissionManager);
+    _createVotingPermissions(_acl, _voting, _voting, _createVotesGrantee, _permissionManager);
   }
 
   function _validateVotingSettings(uint64[3] memory _votingSettings) private pure {
